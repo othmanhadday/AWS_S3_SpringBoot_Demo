faf8f04288cf96495e648fd9d65811d349ada9f7
HADDAY <othmane.hadday@capgemini.com>
2023-07-23 16:23:07 +0100

    first commit

---
+++ src/main/java/com/oth/demo_aws_s3/DemoAwsS3Application.java
@@ -0,0 +1,13 @@
+package com.oth.demo_aws_s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAwsS3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAwsS3Application.class, args);
+	}
+
+}
